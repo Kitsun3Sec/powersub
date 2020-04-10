88aabbed2382b347a4923ea8e1e35793f2547dd3
Alan Lacerda a.k.a. alacerda <alancordeiro@gmail.com>
2020-04-10 04:56:34 -0400

    powersub.py: Catching SIGINT

--- powersub.py
+++ powersub.py
@@ -1,7 +1,7 @@
 import requests
 import sys, os
 import getopt
-import threading
+import threading, signal
 import queue
 
 targetDomain = ''       # Placeholder for the target domain
@@ -16,22 +16,25 @@ def show_help():
     sys.exit()
 
 # Try to connect to HTTP port 80 and 443 in order to test for the existence of the subdomain
-def testSub(subdomain):
+def testSub():
+    global q
     proto = ['http','https']
-    try:
-        # Loop the range of protocols (HTTP and HTTPS)
-        for p in range(len(proto)):
-            # Try to connect to the subdomain on each of the default ports for HTTP and HTTPS
-            r = requests.get(proto[p] + '://' + subdomain + '.' + targetDomain, timeout=(1,3))
-            # If the HTTP status code of the answer is not in the exclude list, we print the subdomain on the screen
-            if str(r.status_code) not in excludeReturn:
-                print(str(r.status_code) + " =>\t" + proto[p] + '://' + subdomain + '.' + targetDomain)
-    except :
-        pass
+    while True:
+        subdomain = q.get()
+        try:
+            # Loop the range of protocols (HTTP and HTTPS)
+            for p in range(len(proto)):
+                # Try to connect to the subdomain on each of the default ports for HTTP and HTTPS
+                r = requests.get(proto[p] + '://' + subdomain + '.' + targetDomain, timeout=(1,3))
+                # If the HTTP status code of the answer is not in the exclude list, we print the subdomain on the screen
+                if str(r.status_code) not in excludeReturn:
+                    print(str(r.status_code) + " =>\t" + proto[p] + '://' + subdomain + '.' + targetDomain)
+        except :
+            pass
+        q.task_done()
 
 def goingForTheKill():
     global q
-    threads = []
 
     if os.path.exists(wordlistFile):
         with open(wordlistFile,"r") as fd:
@@ -40,18 +43,25 @@ def goingForTheKill():
                 q.put(line)
     else:
         print("Wordlist doesn't exist.")
-    while q.qsize() > 0:
-        for instance in range(numberOfThreads):
-            if q.qsize() >= 1:
-                thread = threading.Thread(target = testSub, args = (q.get(),))
-                threads.append(thread)
-                thread.start()
-                print(str(q.qsize()) + " attempts left.", end="\r")
-        for t in threads:
-            t.join()
-    
+
+    try:
+        for i in range(numberOfThreads):
+            t = threading.Thread(target=testSub, daemon=True)
+            t.start()
+
+        while not q.empty():
+            print(str(q.qsize()) + " attempts left.", end="\r")
+
+    except KeyboardInterrupt:
+        sys.exit()
+
+
+def signal_handler(signal, frame):
+    sys.exit()
+
 def main(argv):
-    global targetDomain, wordlistFile, excludeReturn
+    global targetDomain, wordlistFile, excludeReturn, numberOfThreads
+    signal.signal(signal.SIGINT, signal_handler)
 
     # t = target; w = wordlist; h = help; T = Threads; x = exclude
     try:
@@ -65,6 +75,8 @@ def main(argv):
             show_help()
         if opt == '-t':
             targetDomain = arg
+        if opt == '-T':
+            numberOfThreads = int(arg)
         if opt == '-w':
             wordlistFile = arg
         if opt == '-x':
